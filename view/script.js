0e84595fd855c9f7d9a6b132ced133cdda1dd4af
Tim Vlasuk <timvlasuk@gmail.com>
2019-01-20 16:44:14 -0800

    practicing on own time. gain some practice integrating front end with back end

--- view/script.js
+++ view/script.js
@@ -1,7 +1,12 @@
 
 
-
+function handleSelection() {
+    $('.create').on('click', function(e) {console.log('create!')})
+    $('.read').on('click', function(e) {console.log('read!')})
+    $('.update').on('click', function(e) {console.log('update!')})
+    $('.delete').on('click', function(e) {console.log('delete!')})
+}
 
 $(document).ready(function(e) {
-    
+    handleSelection()
 })
