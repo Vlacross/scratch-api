1c7618cb134cb43749a766c4c04d6388d29bc6f2
Tim Vlasuk <timvlasuk@gmail.com>
2019-01-21 09:47:33 -0800

    restructured function/userflow

--- view/script.js
+++ view/script.js
@@ -1,7 +1,73 @@
-let createState = 0;
-let readState = 0;
-let updateState = 0;
-let deleteState = 0;
+function buildListing(post, num) {
+    let listing = 
+    `<li id="${post._id}">
+    <h1>${post.title}</h1>
+    <div>
+        <p>By:${post.author}</p>
+        <p>Date Posted: ${post.created}</p>
+      </div>`
+    if(!num){
+        listing +=
+      `<div>
+    <input class="update" type="submit" value="update">
+    <input class="delete" type="submit" value ="delete">
+    </div>
+    </li>`}
+    return listing;
+}
+
+
+function updatePost(postId) {
+    console.log('updatePoster')
+
+    fetch(`../posts/${postId}`)
+    .then(res => res.json())
+    .then(function(resj) {
+        const currentListing = buildListing(resj, 2)
+        
+    
+
+
+    const updateForm = 
+    `<form>
+      <fieldset>
+        <label>
+          <input class="form-data1" type="text">
+          <input class="formdata-sudmit" type ="submit">
+        </label>
+      </fieldset>
+    </form>`
+
+    $('.js-results').empty().append(currentListing + updateForm)
+    })
+}
+
+
+
+function showPosts() {
+
+    fetch('../posts')
+    .then(res => res.json())
+    .then(function(resj) {
+
+    let blogPosts = ``;
+    resj.forEach(post =>{
+   let listing = buildListing(post)
+
+    blogPosts += listing
+
+    })
+        
+    $('.js-results').append(`<div><ul>${blogPosts}</ul></div>`);
+   })
+   $('.js-results').on('click', '.delete', function(e) {
+       e.preventDefault();
+        currentPost = $(this).closest('li').attr('id')
+        console.log(currentPost)
+        updatePost(currentPost)
+   })
+    
+}
 
 function buildCreate() {
 const createDiv = 
@@ -18,57 +84,55 @@ $('.create-div').append(createDiv);
 };
 
 function buildRead() {
-    const readDiv = 
-    `<form class="read-form"><p class="1">ead</p> <input class="read-input 2" type="text"><input class="read-submit 3" type="submit"></form>`
-    
-if(readState === 1) {
-    readState -= 1;
-$('.read-form').remove();
-}
-else if(readState === 0) {
-    readState += 1;
-$('.read-div').append(readDiv);
-    } 
+showPosts();
 };
 
 function buildUpdate() {
     const updateDiv = 
     `<form class="update-form"><p class="1">pdate</p> <input class="update-input 2" type="text"><input class="update-submit 3" type="submit"></form>`
     
-    if(updateState === 0) {
-        updateState++;
+    if(updateState === 1) {
+        updateState -= 1;
+    $('.update-form').remove();
+    }
+    else if(updateState === 0) {
+        updateState += 1;
     $('.update-div').append(updateDiv);
         } 
-}
+    };
 
 function buildDelete() {
     const deleteDiv = 
     `<form class="delete-form"><p class="1">elete</p> <input class="delete-input 2" type="text"><input class="delete-submit 3" type="submit"></form>`
     
-    if(deleteState === 0) {
-        deleteState++;
+    if(deleteState === 1) {
+        deleteState -= 1;
+    $('.delete-form').remove();
+    }
+    else if(deleteState === 0) {
+        deleteState += 1;
     $('.delete-div').append(deleteDiv);
         } 
-}
+    };
 
 
 function handleSelection() {
-    $('.create').on('click', function(e) {
+    $('.c').on('click', function(e) {
         console.log('create!')
         buildCreate();
     });
 
-    $('.read').on('click', function(e) {
-        console.log('read!')
+    $('.r').on('click', function(e) {
+        console.log('reader!')
         buildRead();
-    })
+    });
 
-    $('.update').on('click', function(e) {
+    $('.u').on('click', function(e) {
         console.log('update!')
         buildUpdate();
     })
 
-    $('.delete').on('click', function(e) {
+    $('.d').on('click', function(e) {
         console.log('delete!')
         buildDelete();
     })
