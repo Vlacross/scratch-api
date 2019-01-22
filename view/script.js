83c4c91cf6ad1f36bf559dae0b77fb51903de4a1
Tim Vlasuk <timvlasuk@gmail.com>
2019-01-21 16:42:21 -0800

    playing around with reduce() and serializeArray() to build header from update Form

--- view/script.js
+++ view/script.js
@@ -1,10 +1,12 @@
 function buildListing(post, num) {
+    
     let listing = 
     `<li id="${post._id}">
     <h1>${post.title}</h1>
     <div>
-        <p>By:${post.author}</p>
+        <p>By: ${post.author}</p>
         <p>Date Posted: ${post.created}</p>
+        <p>${post.content}</p>
       </div>`
     if(!num){
         listing +=
@@ -16,6 +18,39 @@ function buildListing(post, num) {
     return listing;
 }
 
+function buildForm(post) {
+    let {firstName, lastName} = post.author;
+    let name = firstName + ' ' + lastName;
+
+    const updateForm = 
+    `<form class="post-form">
+      <fieldset>
+      <h3>Make your adjustments for posting</h3>
+      <div class="form-inputs">
+        <label for="title-input">Title: 
+          <input id="title-input" class="form-data1" name="title" type="text" value=${post.title}>
+        </label>
+
+        <label for="author-input">Author: 
+          <input id="author-input" class="form-data1" name="author" type="text" value="${name}">
+        </label>
+
+        <input class="put" type ="submit">
+
+      </div>
+
+      <div class="content-input">
+        <label for="content-input">Content: 
+          <textarea id="content-input" class="form-textbox" cols="40" rows="10" type="text" name="content">${post.content}</textarea>
+        </label>
+      </div>
+
+        
+      </fieldset>
+    </form>`
+    return updateForm;
+}
+
 
 function updatePost(postId) {
     console.log('updatePoster')
@@ -23,26 +58,40 @@ function updatePost(postId) {
     fetch(`../posts/${postId}`)
     .then(res => res.json())
     .then(function(resj) {
-        const currentListing = buildListing(resj, 2)
+
+    const updateForm = buildForm(resj)
+
+    $('.wrapper').addClass('row')
+    $('.js-results').empty().append(updateForm)})
+
+    $('.js-results').submit('.post-form', function(e) {
+        e.preventDefault();
         
+        const form = $(event.target);
+        let formData = form.serializeArray()
+        
+        function buildHead(formData) {
+            return formData.reduce(function(acc, obj) {
+
+                acc[obj.name] = obj.value
+                return acc
+            }, {})
+        }
+
+        let head = buildHead(formData);
+        console.log(JSON.stringify(head))
+        // fetch(`../posts/${postId}`, {method: 'PUT'})
+        
+        // .then(resj => console.log(resj.status))
+        })
     
-
-
-    const updateForm = 
-    `<form>
-      <fieldset>
-        <label>
-          <input class="form-data1" type="text">
-          <input class="formdata-sudmit" type ="submit">
-        </label>
-      </fieldset>
-    </form>`
-
-    $('.js-results').empty().append(currentListing + updateForm)
-    })
 }
 
+function deletePost(postId) {
+    fetch(`../posts/${postId}`, {method: 'delete'})
+    .then(res => console.log(res.status))
 
+}
 
 function showPosts() {
 
@@ -52,35 +101,32 @@ function showPosts() {
 
     let blogPosts = ``;
     resj.forEach(post =>{
-   let listing = buildListing(post)
+    let listing = buildListing(post)
 
     blogPosts += listing
 
     })
         
-    $('.js-results').append(`<div><ul>${blogPosts}</ul></div>`);
+    $('.js-results').empty().append(`<div><ul>${blogPosts}</ul></div>`);
    })
-   $('.js-results').on('click', '.delete', function(e) {
+
+   $('.js-results').on('click', '.update', function(e) {
        e.preventDefault();
         currentPost = $(this).closest('li').attr('id')
         console.log(currentPost)
         updatePost(currentPost)
    })
+
+   $('.js-results').on('click', '.delete', function(e) {
+    e.preventDefault();
+     currentPost = $(this).closest('li').attr('id')
+     console.log(currentPost)
+     deletePost(currentPost)
+})
     
 }
 
 function buildCreate() {
-const createDiv = 
-` <form class="create-form"><p class="1">reate</p> <input class="create-input 2" type="text"><input class="create-submit 3" type="submit"></form>`
-
-if(createState === 1) {
-    createState -= 1;
-$('.create-form').remove();
-}
-else if(createState === 0) {
-    createState += 1;
-$('.create-div').append(createDiv);
-    } 
 };
 
 function buildRead() {
@@ -88,52 +134,33 @@ showPosts();
 };
 
 function buildUpdate() {
-    const updateDiv = 
-    `<form class="update-form"><p class="1">pdate</p> <input class="update-input 2" type="text"><input class="update-submit 3" type="submit"></form>`
-    
-    if(updateState === 1) {
-        updateState -= 1;
-    $('.update-form').remove();
-    }
-    else if(updateState === 0) {
-        updateState += 1;
-    $('.update-div').append(updateDiv);
-        } 
-    };
+};
 
 function buildDelete() {
-    const deleteDiv = 
-    `<form class="delete-form"><p class="1">elete</p> <input class="delete-input 2" type="text"><input class="delete-submit 3" type="submit"></form>`
-    
-    if(deleteState === 1) {
-        deleteState -= 1;
-    $('.delete-form').remove();
-    }
-    else if(deleteState === 0) {
-        deleteState += 1;
-    $('.delete-div').append(deleteDiv);
-        } 
-    };
-
+};
 
 function handleSelection() {
     $('.c').on('click', function(e) {
         console.log('create!')
+        $('.intro').remove();
         buildCreate();
     });
 
     $('.r').on('click', function(e) {
         console.log('reader!')
+        $('.intro').remove();
         buildRead();
     });
 
     $('.u').on('click', function(e) {
         console.log('update!')
+        $('.intro').remove();
         buildUpdate();
     })
 
     $('.d').on('click', function(e) {
         console.log('delete!')
+        $('.intro').remove();
         buildDelete();
     })
 
