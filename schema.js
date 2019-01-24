bfa8c6bce84541bc496e53ca774581b7c6b073a5
Tim Vlasuk <timvlasuk@gmail.com>
2019-01-24 15:50:16 -0800

    having difficulty manipulating data to return in proper format. Between using schema toObj option- .set('toObj')- and returning w/ method. Also built Author and Comment seperately for now.

--- schema.js
+++ schema.js
@@ -1,43 +1,85 @@
 const mongoose = require('mongoose');
 const schema = mongoose.Schema;
+const ObjectId = schema.Types.ObjectId;
+mongoose.set('useCreateIndex', true)
+
+
+
+
+const SCHEMA_OPTS = {
+  toObject: {
+    virtuals: true,
+    transform: function (doc, ret) {
+      delete ret._id;
+      delete ret.__v;
+    }
+  }
+}
+
+
+
+const commentSchema = new schema({
+  content: String,
+  date: Date,
+}, {default: undefined})
+
+
+
+
+
+
 
 const blogPostSchema = new schema({
   title: String,
-  author: {
-    firstName: String,
-    lastName: String
-  },
+  author: {type: ObjectId, ref: 'Author'},        
   content: String,
-  created: Date
-}, {toObject: {
-  transform: function(doc, ret) {
-    ret.id = ret._id;
-    delete ret._id;
-    delete ret.__v
+  created: {type: Date, default: Date.now},
+  comments: [commentSchema]
+}, SCHEMA_OPTS);
+
+blogPostSchema.methods.serialize = function() {
+  return {
+    title: this.title,
+    author: this.authrah,
+    content: this.content,
+    created: this.created,
+    comments: this.comments
   }
-}});
+}
 
-
-
-blogPostSchema.virtual('fullname').get(function() {
+blogPostSchema.virtual('authrah').get(function() {
   return this.author.firstName + ' ' + this.author.lastName
 });
 
-// blogPostSchema.set('toObject', {
-//   transform: function (doc, ret, options) {
-//     ret.id = ret._id;
+const authorSchema = new schema({
+  firstName: String,
+  lastName: String,
+  userName: {type: String,
+             unique: true}
+}, SCHEMA_OPTS);
+
+// authorSchema.set('toObject', {
+//   transform: function(doc, ret){
 //     delete ret._id;
-//     delete ret.__v;
+    
+
 //   }
-// });
+// })
 
-const Post = mongoose.model('post', blogPostSchema);
+authorSchema.virtual('fullName').get(function() {
+  return this.firstName + ' ' + this.lastName
+})
+
+
+
+const Comment = mongoose.model('Comment', commentSchema);
+const Post = mongoose.model('Post', blogPostSchema);
+const Author = mongoose.model('Author', authorSchema)
 
 
 
 
-
-module.exports = { Post }
+module.exports = { Post, Author, Comment }
 
 /*
 
