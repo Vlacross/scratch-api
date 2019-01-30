c919ba92d3b7ef88db048f53430c92a8233bf4f3
Tim Vlasuk <timvlasuk@gmail.com>
2019-01-29 20:44:12 -0800

    saving snippets during dev process

--- toolCode.js
+++ toolCode.js
@@ -14,3 +14,20 @@ function extract(rawVal) {
 
 
 module.exports = { extract }
+
+
+
+
+
+
+  // Post.findByIdAndUpdate({ _id: mongoose.Types.ObjectId() }, {
+    //     title: req.body.title,
+    //     author: newId,
+    //     content: req.body.content,
+    //     created: new Date
+    // }, {
+    //         upsert: true,
+    //         setDefaultsOnInsert: true,
+    //         new: true,
+    //         populate: 'author'
+    //     })
